4e1a6e20a9605fd72ed982e5c1eadc8f9d2f5b43
caseyjoneal <caseyjoneal@gmail.com>
2016-11-16 15:32:47 -0800

    sanity check for app.js and DOM

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,2 +1,20 @@
-// connection test
-console.log("I'm connected");
+console.log("app.js is connected");
+
+$(document).ready(function(){
+console.log('DOM is ready! ');
+
+// $.ajax({
+//   method: 'GET',
+//   url: '',
+//   type: 'jsonData'
+//   success: handleSuccess,
+//   error: handleError
+//
+// }) // ajax closer
+
+}); //document closer TODO: remove before production
+
+
+// function handleSuccess(jsonData){
+//   console.log(jsonData);
+// };
