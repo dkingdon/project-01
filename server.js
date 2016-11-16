a195dbe2d4698e145bdf8cbcc82aaee19c58bd89
caseyjoneal <caseyjoneal@gmail.com>
2016-11-16 15:31:39 -0800

    routed index/server is running

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var controllers = require('./controllers');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname + '/public'));
+
+
+
+// HTML endpoints
+app.get('/', function homepage (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// listen on port 3000
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Yea boi! Express server is running on http://localhost:3000/');
+});
