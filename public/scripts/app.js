f5047e337331b7d04eedc9b866b859557403c5ef
caseyjoneal <caseyjoneal@gmail.com>
2016-11-17 15:26:44 -0800

    correct indentation?

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,35 +1,32 @@
 console.log("app.js is connected");
 
-$(document).ready(function(){
-console.log('DOM is ready!');
+$(document)
+  .ready(function(){
+    console.log('DOM is ready!');
 
-$.ajax({
-    method: 'GET',
-    url: '/api/trails',
-    type: 'jsonData',
-    success: handleSuccess,
-    error: handleError
-})
+    $.ajax({
+      method: 'GET',
+      url: '/api/trails',
+      type: 'jsonData',
+      success: handleSuccess,
+      error: handleError
+    })
+ }); //document closer TODO: remove before production
 
-}); //document closer TODO: remove before production
-
-function handleSuccess(jsonData){
-    console.log('? this is >> ', jsonData);
+  function handleSuccess(jsonData){
     var trails = jsonData.trails;
     trails.forEach(function (trailIndex){
     renderTrails(trailIndex)
     });
+  }
 
-}
+  function handleError(){
+    console.log('Ajax'+'"GET"'+' ERROR!');
+  }
 
-function handleError(){
-  console.log('Ajax'+'"GET"'+' ERROR!');
-}
-
-// fn that handles compiling for handlebars
-function renderTrails(trail){
-  var source = $('#result-template').html(),
-   template = Handlebars.compile(source),
-   trail = template(trail);
-   $('#trail-target').prepend(trail);
-}
+  function renderTrails(trail){
+    var source = $('#result-template').html(),
+    template = Handlebars.compile(source),
+    trail = template(trail);
+    $('#trail-target').prepend(trail);
+  }
