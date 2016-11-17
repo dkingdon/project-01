b0fbace79d89114af19d1d49f0f0269f9edc1596
caseyjoneal <caseyjoneal@gmail.com>
2016-11-16 17:13:02 -0800

    skeleton code for ajax clbks and handle bars fn

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,11 +1,11 @@
 console.log("app.js is connected");
 
 $(document).ready(function(){
-console.log('DOM is ready! ');
+console.log('DOM is ready!');
 
 // $.ajax({
 //   method: 'GET',
-//   url: '',
+//   url: '/api/trails',
 //   type: 'jsonData'
 //   success: handleSuccess,
 //   error: handleError
@@ -15,7 +15,18 @@ console.log('DOM is ready! ');
 }); //document closer TODO: remove before production
 
 
-// function handleSuccess(jsonData){
-//   console.log(jsonData);
-// };
-//
+function handleSuccess(jsonData){
+  console.log('? this is >>'+ jsonData);
+}
+
+function handleError(){
+  console.log('Ajax'+'"GET"'+' ERROR!');
+}
+
+// fn that handles compiling for handlebars
+function renderTrails(trail){
+  source = $('#trail-template').html
+  template = Handlebars.compile(source)
+  var trail = template(trail)
+  $('#trail-target').prepend(trail)
+};
