c150139f0f6948ed38b191342f0b84eb7a0db202
caseyjoneal <caseyjoneal@gmail.com>
2016-11-21 18:06:57 -0800

    modifying files for heroku push

--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/project-01");
+ mongoose.connect( process.env.MONGODB_URI || "connecting to heroku" );
+// mongoose.connect("mongodb://localhost/project-01");
 module.exports.Trail = require("./trail.js");
 module.exports.UserComments = require("./userComments.js");
