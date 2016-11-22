ec98a476891b592a614a8ca16039894c1cbfcd47
caseyjoneal <caseyjoneal@gmail.com>
2016-11-21 18:33:55 -0800

    resolving heroku error

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose");
- mongoose.connect( process.env.MONGODB_URI || "connecting to heroku" );
+ mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/project-01" );
 // mongoose.connect("mongodb://localhost/project-01");
 module.exports.Trail = require("./trail.js");
 module.exports.UserComments = require("./userComments.js");
